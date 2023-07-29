2eb5fb25e256a1be156ed348ae5d1ca07ddfc86f
EC2 Default User <ec2-user@ip-172-31-19-78.ap-northeast-1.compute.internal>
2023-07-29 14:15:28 +0000

    7/29

--- app/controllers/uesrs_controller.rb
+++ app/controllers/uesrs_controller.rb
@@ -1,11 +1,28 @@
 class UesrsController < ApplicationController
   
-  
+  def index
+    @users = User.all
+  end
   
   def show
     @user = User.find(params[:id])
+    @books = current_user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+  
 end
